8506f73dec32e9eb4136cdd39864ab5dd57a8aff
mgrsato <mgrsato@gmail.com>
2019-09-02 15:42:34 +0900

    Update weather.sh

--- weather.sh
+++ weather.sh
@@ -1,17 +1,18 @@
-#!/bin/sh
+#!/usr/bin/sh
 
 wget -q https://weathernews.jp/onebox/35.455618/139.629826/temp=c -O /dev/stdout > temp
 
-obs_time=`grep -o "<span class=\"sub\"[^>]*>[^<]*</span>" temp | sed -e "s/<span.*>\(.*\)<\/span>/\1/" | cut -d "," -f 1`
-obs_weather=`grep -o "<span class=\"sub\"[^>]*>[^<]*</span>" temp | sed -e "s/<span.*>\(.*\)<\/span>/\1/" | cut -d " " -f 2`
-obs_temperature=`grep -o "<span class=\"obs_temp_main\"[^>]*>[^<]*</span>" temp | sed -e "s/<span.*>\(.*\)<\/span>/\1/"`
-obs_humidity=`grep -o "<td class=\"obs_sub_value\"[^>]*>[^<]*</td>" temp | sed -e "s/<td.*>\(.*\)<\/td>/\1/" | cut -d " " -f 3 | head -n 1`
-obs_pressure=`grep -o "<td class=\"obs_sub_value\"[^>]*>[^<]*</td>" temp | sed -e "s/<td.*>\(.*\)<\/td>/\1/" | cut -d " " -f 3 | head -n 2 | tail -n 1`
-obs_wind_direction=`grep -o "<td class=\"obs_sub_value\"[^>]*>[^<]*</td>" temp | sed -e "s/<td.*>\(.*\)<\/td>/\1/" | cut -d " " -f 3 | head -n 3 | tail -n 1`
-obs_wind_speed=`grep -o "<td class=\"obs_sub_value\"[^>]*>[^<]*</td>" temp | sed -e "s/<td.*>\(.*\)<\/td>/\1/" | cut -d " " -f 4 | head -n 3 | tail -n 1`
+obs_time=`grep -A 1 '<div class="weather-now__cont">' temp | sed -e "s/ *//" | sed -n 2p | sed -e "s/<p.*>\(.*\)<\/p>/\1/"`
+obs_weather=`grep '<span class="tit">Weather</span>' temp | sed -e "s/ *//" | sed -e "s/<li.*>\(.*\)<\/li>/\1/"`
+obs_temperature=`grep '<span class="tit">Temp.</span>' temp | sed -e "s/ *//" | sed -e "s/<li.*>\(.*\)<img.*><\/li>/\1/" | sed -e "s/.$//"`
+obs_humidity=`grep '<span class="tit">RH</span>' temp | sed -e "s/ *//" | sed -e "s/<li.*>\(.*\)<\/li>/\1/" | sed -e "s/.$//"`
+obs_pressure=`grep '<span class="tit">Pres.</span>' temp | sed -e "s/ *//" | sed -e "s/<li.*>\(.*\)<\/li>/\1/" | sed -e "s/...$//"`
+obs_wind_direction=`grep '<span class="tit">Wind　</span>' temp | sed -e "s/ *//" | sed -e "s/<li.*>\(.*\)<\/li>/\1/" | cut -d " " -f 1`
+obs_wind_speed=`grep '<span class="tit">Wind　</span>' temp | sed -e "s/ *//" | sed -e "s/<li.*>\(.*\)<\/li>/\1/" | cut -d " " -f 2 | sed -e "s/...$//"`
 
+#画面に出力
 echo "＊＊＊現在の天気＊＊＊"
-echo "時刻："$obs_time
+echo "データ取得時刻："$obs_time
 echo "天気："$obs_weather
 echo "気温："$obs_temperature
 echo "湿度："$obs_humidity
